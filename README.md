65589bb735f70f9c449ddab7b30373ae16a30699
marusho <mrsho34@gmail.com>
2021-06-04 23:32:19 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # emoji-slot
+
+## refs
+
+- https://stackoverflow.com/questions/62426257/creating-an-animation-for-a-basic-react-slot-machine
+- https://codepen.io/antibland/pen/ypagZd
+- https://gist.github.com/staven630/a2b04b8363d3be5b1ad1834d538ddadd
---
+++ emoji-slot/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+
+//import Game from "./components/Game";
+import Slots from "./components/Slot";
+
+//ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<Slots />, document.getElementById("root"));
+
+
