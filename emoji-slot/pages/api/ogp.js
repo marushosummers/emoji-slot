ef4244094f702de2b3fda3480ef95681443993ca
marusho <mrsho34@gmail.com>
2021-06-18 16:32:09 +0900

    OGPをパラメータから生成する

--- emoji-slot/pages/api/ogp.js
+++ emoji-slot/pages/api/ogp.js
@@ -1,33 +1,45 @@
 import { createCanvas } from "canvas";
 import { fillTextWithTwemoji } from "node-canvas-with-twemoji";
 
-export default async (
-	request,
-	response
-) => {
-	const buf = await createOGP();
-
-	response.writeHead(200, {
-		"Content-Type": "image/png",
-		"Content-Length": buf.length,
-	});
-	response.end(buf, "binary");
+export default async (request, response) => {
+	try{
+		const parameters = getPrameters(request.query);
+		const buf = await createOGP(parameters);
+		response.writeHead(200, {
+			"Content-Type": "image/png",
+			"Content-Length": buf.length,
+		});
+		response.end(buf, "binary");
+	} catch(e) {
+		response.writeHead(500, {
+			"Content-Type": "text/html",
+		});
+		response.end("Internal Server Error");
+	}
 };
 
-const createOGP = async () => {
+const getPrameters = (query) => {
+	return {
+		p1: query.p1 ?? "🎰",
+		p2: query.p2 ?? "🎰",
+		p3: query.p3 ?? "🎰",
+	};
+}
+
+const createOGP = async (parameters) => {
 	const W = 1200;
 	const H = 630;
 
 	const canvas = createCanvas(W, H);
 	const ctx = canvas.getContext("2d");
 
-  // backgroud color
-  ctx.fillStyle = "#F7F7F7";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+	// backgroud color
+	ctx.fillStyle = "#F7F7F7";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 	// The size of the emoji
 	ctx.font = "120px serif";
-  ctx.fillStyle = "#000000";
+	ctx.fillStyle = "#000000";
 
 	// use positioning
 	ctx.textAlign = "center";
@@ -36,19 +48,19 @@ const createOGP = async () => {
 	// draw the emoji
 	await fillTextWithTwemoji(
 		ctx,
-		"🎰",
+		parameters.p1,
 		(canvas.width / 6) * 2,
 		canvas.height / 2
 	);
 	await fillTextWithTwemoji(
 		ctx,
-		"🎰",
+		parameters.p3,
 		(canvas.width / 6) * 3,
 		canvas.height / 2
 	);
 	await fillTextWithTwemoji(
 		ctx,
-		"🎰",
+		parameters.p2,
 		(canvas.width / 6) * 4,
 		canvas.height / 2
 	);
