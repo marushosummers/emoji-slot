eee9bce7844e0c026547ce79e3cc634a72c58d2d
marusho <mrsho34@gmail.com>
2021-06-20 23:14:00 +0900

    feat: gatag

--- emoji-slot/pages/_app.js
+++ emoji-slot/pages/_app.js
@@ -1,4 +1,8 @@
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
 import "../src/styles/globals.css";
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
