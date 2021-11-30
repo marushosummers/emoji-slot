edd8235d6796b5587ff06b2a06accb161c157e27
marusho <mrsho34@gmail.com>
2021-11-30 21:23:16 +0900

    不要なRouterを削除

--- emoji-slot/pages/index.js
+++ emoji-slot/pages/index.js
@@ -1,15 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Slot from '../components/Slot'
-import { useRouter } from 'next/router';
 
 export async function getServerSideProps({ query }) {
 	return { props: { query } };
 }
 
-
 export default function Home(props) {
-  const router = useRouter();
 
 	return (
 		<div className={styles.container}>
